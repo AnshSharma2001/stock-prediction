9cd7c11166186bd20883cfa4ee36f8567b2d726a
AnshSharma2001 <vanshsethi11@gmail.com>
2024-04-06 04:06:12 -0500

    Added a broilerplate section for settings

---
+++ src/app/(protected)/settings/layout.tsx
@@ -0,0 +1,20 @@
+import { Sidebar } from "@/components/setting/sidebar";
+import React from "react";
+
+const SettingsLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        <div className="mx-auto grid w-full max-w-6xl gap-2">
+          <h1 className="text-3xl font-semibold">Settings</h1>
+        </div>
+        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
+          <Sidebar />
+          {children}
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default SettingsLayout;
