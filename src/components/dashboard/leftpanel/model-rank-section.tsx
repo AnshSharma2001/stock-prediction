6c183e3aa75ce4bbdf6d8cfc90edcdae4df3c65c
JoeyHussain <Joey.Hussain@mavs.uta.edu>
2024-04-09 14:57:52 -0500

    Update model-rank-section.tsx

--- src/components/dashboard/leftpanel/model-rank-section.tsx
+++ src/components/dashboard/leftpanel/model-rank-section.tsx
@@ -1,4 +1,20 @@
+"use client";
+
+import React, { useState, useTransition, useEffect } from "react";
 import { ModelCategoryCarousel } from "./model-category-carousel";
+import fetch from 'node-fetch';
+
+export const getMseRanking = async () => {
+    const response = await fetch('http://3.129.67.70/general/models/average_mse_by_model_and_timeframe');
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const mseRankingData = await response.json();
+    return mseRankingData;
+}
+
 
 const sampleDataFromAPI = {
   models: [
@@ -10,51 +26,57 @@ const sampleDataFromAPI = {
       Name: "AdaBoost.joblib",
       Subscribe_Count: 1,
       UserID: 1,
+      Tags: [1,2]
     },
     {
       Description: "This is a test for GradientBoosting.joblib",
-      Like_Count: 0,
+      Like_Count: 8,
       Model_File_Path: "/home/ec2-user/stock_models/1_GradientBoosting.joblib",
       Model_ID: 2,
       Name: "GradientBoosting.joblib",
-      Subscribe_Count: 0,
+      Subscribe_Count: 8,
       UserID: 1,
+      Tags: [1,2]
     },
     {
       Description: "This is a test for LGBMRegressor.joblib",
-      Like_Count: 0,
+      Like_Count: 4,
       Model_File_Path: "/home/ec2-user/stock_models/1_LGBMRegressor.joblib",
       Model_ID: 3,
       Name: "LGBMRegressor.joblib",
-      Subscribe_Count: 0,
+      Subscribe_Count: 4,
       UserID: 1,
+      Tags: [1,2]
     },
     {
       Description: "This is a test for RandomForest.joblib",
-      Like_Count: 0,
+      Like_Count: 2,
       Model_File_Path: "/home/ec2-user/stock_models/1_RandomForest.joblib",
       Model_ID: 4,
       Name: "RandomForest.joblib",
-      Subscribe_Count: 0,
+      Subscribe_Count: 2,
       UserID: 1,
+      Tags: [1,2]
     },
     {
       Description: "This is a test for DecisionTree.joblib",
-      Like_Count: 0,
+      Like_Count: 9,
       Model_File_Path: "/home/ec2-user/stock_models/1_DecisionTree.joblib",
       Model_ID: 5,
       Name: "DecisionTree.joblib",
-      Subscribe_Count: 0,
+      Subscribe_Count: 9,
       UserID: 1,
+      Tags: [1,2,3]
     },
     {
       Description: "This is a test for KNeighbors.joblib",
-      Like_Count: 0,
+      Like_Count: 25,
       Model_File_Path: "/home/ec2-user/stock_models/1_KNeighbors.joblib",
       Model_ID: 6,
       Name: "KNeighbors.joblib",
-      Subscribe_Count: 0,
+      Subscribe_Count: 25,
       UserID: 1,
+      Tags: [2,3]
     },
     {
       Description: "This is a test for LinearRegression.joblib",
@@ -64,6 +86,7 @@ const sampleDataFromAPI = {
       Name: "LinearRegression.joblib",
       Subscribe_Count: 0,
       UserID: 1,
+      Tags: [3]
     },
     {
       Description: "This is a test for SVR.joblib",
@@ -73,48 +96,74 @@ const sampleDataFromAPI = {
       Name: "SVR.joblib",
       Subscribe_Count: 0,
       UserID: 1,
-    },
-    {
-      Description: "This is a test model for Maulik .",
-      Like_Count: 0,
-      Model_File_Path: "/home/ec2-user/stock_models/7_Mauliks Best Model",
-      Model_ID: 10,
-      Name: "Mauliks Best Model",
-      Subscribe_Count: 0,
-      UserID: 7,
-    },
-    {
-      Description: "This is a test model for Maulik .",
-      Like_Count: 0,
-      Model_File_Path: "/home/ec2-user/stock_models/7_Mauliks Best Model",
-      Model_ID: 11,
-      Name: "Mauliks Best Model",
-      Subscribe_Count: 0,
-      UserID: 7,
+      Tags: [3]
     },
   ],
 };
 
+
+const fetchMseRanking = async () => {
+  const mseUrl = 'http://3.129.67.70/general/models/average_mse_by_model_and_timeframe';
+  const response = await fetch(mseUrl);
+  if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  return await response.json();
+};
+
 export const ModelRankSection = () => {
-  // TODO: Ideally would map over here as well.
-  const { models } = sampleDataFromAPI;
+  const [models, setModels] = useState(sampleDataFromAPI.models);
+  const [modelCategories, setModelCategories] = useState({
+      favTech: [],
+      favFinance: [],
+      favHealthcare: [],
+      bestTech: [],
+      bestFinance: [],
+      bestHealthcare: []
+  });
+
+  useEffect(() => {
+      fetchMseRanking().then(data => {
+          const updatedModels = models.map(model => {
+              const mseData = data[model.Model_ID];
+              return { ...model, monthlyMSE: mseData ? mseData["Previous Month"] : null };
+          });
+          setModels(updatedModels);
+      }).catch(error => console.error("Error fetching MSE Ranking:", error));
+  }, []);
+
+  useEffect(() => {
+      // Logic that depends on updated models
+      setModelCategories({
+          favTech: rankModelslikes(filterModelsByTag(models, 1)),
+          favFinance: rankModelslikes(filterModelsByTag(models, 2)),
+          favHealthcare: rankModelslikes(filterModelsByTag(models, 3)),
+          bestTech: rankModelsByMSE(filterModelsByTag(models, 1)),
+          bestFinance: rankModelsByMSE(filterModelsByTag(models, 2)),
+          bestHealthcare: rankModelsByMSE(filterModelsByTag(models, 3))
+      });
+  }, [models]);
+
+  const rankModelsByMSE = (models) => {
+      return [...models].sort((a, b) => a.monthlyMSE - b.monthlyMSE).slice(0, 10);;
+  };
+
+  const filterModelsByTag = (models, tag) => {
+      return models.filter(model => model.Tags.includes(tag));
+  };
+
+  const rankModelslikes = (models) => {
+      return [...models].sort((a, b) => b.Like_Count - a.Like_Count).slice(0, 10);;
+  };
+
   return (
-    <div className="pt-8 grid grid-cols-1 space-y-20 w-full mb-10">
-      <ModelCategoryCarousel rankingTitle="Trending in Tech" models={models} />
-      <ModelCategoryCarousel
-        rankingTitle="Trending in Finance"
-        models={models}
-      />
-      <ModelCategoryCarousel
-        rankingTitle="High risk, high reward"
-        models={models}
-      />
-      <ModelCategoryCarousel rankingTitle="Slow and Steady" models={models} />
-      <ModelCategoryCarousel
-        rankingTitle="Trending in Commodity"
-        models={models}
-      />
-      <ModelCategoryCarousel rankingTitle="Trending in Tech" models={models} />
-    </div>
+      <div className="pt-8 grid grid-cols-1 space-y-20 w-full mb-10">
+          <ModelCategoryCarousel rankingTitle="Favorites in Tech" models={modelCategories.favTech} />
+          <ModelCategoryCarousel rankingTitle="Favorites in Finance" models={modelCategories.favFinance} />
+          <ModelCategoryCarousel rankingTitle="Favorites in Health Care" models={modelCategories.favHealthcare} />
+          <ModelCategoryCarousel rankingTitle="Best Monthly for Tech" models={modelCategories.bestTech} />
+          <ModelCategoryCarousel rankingTitle="Best Monthly for Finance" models={modelCategories.bestFinance} />
+          <ModelCategoryCarousel rankingTitle="Best Monthly for Health Care" models={modelCategories.bestHealthcare} />
+      </div>
   );
 };
