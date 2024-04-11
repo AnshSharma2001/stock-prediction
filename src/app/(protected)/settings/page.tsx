5b435c744d37a4360da21b2ab423127df484860e
sehgalmaulik <sehgalmaulik@gmail.com>
2024-04-10 21:07:48 -0500

    try 5

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,10 +1,23 @@
 "use client";
 
+import { set } from "date-fns";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 const SettingsPage = () => {
   const router = useRouter();
-  return router.replace("/settings/general");
+  const [mounted, setMounted] = useState(false);
+  
+  useEffect(() => {
+    setMounted(true);
+  }, [])
+
+  if (!mounted) {
+    return null;
+  }
+
+    return router.replace("/settings/general");
+//   return router.replace("/settings/general");
 };
 
 export default SettingsPage;
