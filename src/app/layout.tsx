75a5673c26365d59cfa550c3a7c80f00163e9ae8
sehgalmaulik <sehgalmaulik@gmail.com>
2024-04-10 20:49:43 -0500

    huy part 3

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 });
