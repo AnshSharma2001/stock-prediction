569250c109e205c3b060be4f61edfd218421f57a
sehgalmaulik <sehgalmaulik@gmail.com>
2024-04-10 20:53:55 -0500

    part4

--- auth.config.ts
+++ auth.config.ts
@@ -1,9 +1,7 @@
 import Credentials from "next-auth/providers/credentials";
 import type { NextAuthConfig } from "next-auth";
-import bcryptjs from "bcryptjs";
 import { LoginSchema } from "./schemas";
-import { getUserByEmail } from "./data/user";
-import { access } from "fs";
+
 
 export default {
   providers: [
