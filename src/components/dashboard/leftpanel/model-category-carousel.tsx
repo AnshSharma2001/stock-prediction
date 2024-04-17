af8f6102842cd5b3089e6ba8001299c23f48d8b8
JoeyHussain <Joey.Hussain@mavs.uta.edu>
2024-04-17 00:37:46 -0500

    Update Model Rank System

--- src/components/dashboard/leftpanel/model-category-carousel.tsx
+++ src/components/dashboard/leftpanel/model-category-carousel.tsx
@@ -16,6 +16,7 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { BaseCard } from "@/components/model-card/base-card";
 
+
 interface Model {
   Description: string;
   Like_Count: number;
@@ -24,6 +25,11 @@ interface Model {
   Name: string;
   Subscribe_Count: number;
   UserID: number;
+  Model_Name: string;
+  dailyMSE?: number;
+  monthlyMSE?: number;
+  yearlyMSE?: number;
+  Tags?: string[];
 }
 
 interface ModelCategoryCarouselProps {
@@ -37,28 +43,33 @@ export const ModelCategoryCarousel = ({
 }: ModelCategoryCarouselProps) => {
   // TODO: UPDATE THE LIKES IN THE BACKEND AND REFLECT THAT ON EVERY MODEL BY DEFAULT
 
+  
+
   return (
-    <div className="h-72">
+    <div className="w-full h-72">
       <h2 className="text-xl font-semibold">{rankingTitle}</h2>
       <Separator className="my-4 w-full" />
       <Carousel
         opts={{
           align: "start",
         }}
-        // className="w-full"
+        className="w-full"
       >
         <CarouselContent>
           {models.map((model) => (
-            <CarouselItem key={model.Model_ID} className=" basis-1/3">
-              <BaseCard
-                Description={model.Description}
-                Like_Count={model.Like_Count}
-                Model_File_Path={model.Model_File_Path}
-                Model_ID={model.Model_ID}
-                Name={model.Name}
-                Subscribe_Count={model.Subscribe_Count}
-                UserID={model.UserID}
-              />
+            <CarouselItem
+              key={model.Model_ID}
+              className="md:basis-1/3 lg:basis-1/3 xl:basis-1/3"
+            >
+                <BaseCard
+                 Description={model.Description}
+                 Like_Count={model.Like_Count}
+                    Model_File_Path={model.Model_File_Path}
+                    Model_ID={model.Model_ID}
+                    Name={model.Name}
+                    Subscribe_Count={model.Subscribe_Count}
+                    UserID={model.UserID}
+                 />
             </CarouselItem>
           ))}
         </CarouselContent>
