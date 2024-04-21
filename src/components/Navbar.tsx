b739b5f7972f3ec3c9e1b73fe0e1f5a49fa50d8c
JoeyHussain <Joey.Hussain@mavs.uta.edu>
2024-04-21 00:25:57 -0500

    Merge branch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,8 +20,9 @@ import { Button } from "@/components/ui/button";
 import { CommandMenu } from "./ui/command-menu";
 import DropDownNav from "./dropdown-nav";
 import { usePathname } from "next/navigation";
-import { set } from "date-fns";
 
+// import { Dialog, DialogTrigger } from "@radix-ui/react-dialog";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { PlusIcon } from "@radix-ui/react-icons";
 import {
   TooltipProvider,
@@ -37,21 +38,21 @@ export default function Navbar() {
   const menus = [
     { title: "Dashboard", path: "/dashboard", id: 1 },
     { title: "About", path: "/about", id: 2 },
+    { title: "Models", path: "/view-models", id: 3 },
   ];
 
-  // Function to determine the className based on whether the item is selected
   const getItemClassName = (menuPath: string) => {
     return cn(
-      navigationMenuTriggerStyle(), // Apply default styles
+      navigationMenuTriggerStyle(),
       pathname === menuPath
         ? "bg-accent text-accent-foreground"
-        : "bg-background text-foreground" // Conditional class; change "text-blue-500" and "text-gray-700" as needed
+        : "bg-background text-foreground"
     );
   };
 
   return (
-    <nav className="flex shrink-0 h-20 justify-between items-center px-4 top-0 z-10 backdrop-filter backdrop-blur-md bg-opacity-30">
-      <div className="ms-4">
+    <div className="p-4 flex items-center justify-between relative">
+      <aside className="flex items-center gap-2">
         <Link href="/dashboard">
           <Image
             src={
@@ -62,28 +63,31 @@ export default function Navbar() {
                 : LogoSystem
             }
             alt="Logo"
-            width={50}
-            height={50}
+            width={40}
+            height={40}
           />
         </Link>
-      </div>
-      <div className="relative lg:block lg:w-1/3">
-        <CommandMenu />
-      </div>
-      <div>
-        <NavigationMenu>
-          <NavigationMenuList className="flex gap-2 pr-10">
-            {menus.map((menu) => (
-              <NavigationMenuItem key={menu.id}>
-                <Link href={menu.path} legacyBehavior passHref>
-                  <NavigationMenuLink
-                    className={getItemClassName(menu.path)} // Update selected state
-                  >
-                    {menu.title}
-                  </NavigationMenuLink>
-                </Link>
-              </NavigationMenuItem>
-            ))}
+        <span className="text-xl font-bold">Trademinds</span>
+      </aside>
+
+      <nav
+        className="hidden md:block absolute left-[50%] top-[50%] 
+      transform translate-x-[-50%] translate-y-[-50%]"
+      >
+        <ul className="flex items-center justify-center gap-8">
+          {menus.map((menu) => (
+            <Link href={menu.path} key={menu.id}>
+              <div
+                className={getItemClassName(menu.path)} // Update selected state
+              >
+                {menu.title}
+              </div>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+
+      <aside className=" flex gap-2 items-center">
             <Link href='/create-model' passHref>
               <TooltipProvider>
                   <Tooltip>
@@ -98,11 +102,8 @@ export default function Navbar() {
                   </Tooltip>
                 </TooltipProvider>
             </Link>
-            <DropDownNav />
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
-    </nav>
+        <DropDownNav />
+      </aside>
+    </div>
   );
 }
-
